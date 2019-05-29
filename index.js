03e1f2182ef3b6e8ff0c84577f79f714ad9f4f92
Karol <michalowskikp@gmail.com>
2019-05-30 00:59:03 +0200

    Forbids to create/join new game while in another

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
+/*
+* MESSAGE CODES
+* turn {roomId} {turnNumber} {gameStatus}
+*	gameStatus: 0 -> game in progress
+* gameStatus: 1 -> someone won
+* gameStatus: 2 -> draw 
+*
+* roomStatus:
+* 0 -> pending for enemy
+* 1 -> game in progress
+* 2 -> game ended
+*
+*/
+
+
+
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var roomCounter = 0;
+var roomStatus = [];
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
@@ -21,6 +38,7 @@ io.on('connection', function(socket){
 		
 		console.log('room with id: ' + roomCounter + ' has been created by: ' + msg);
 		socket.join(roomCounter);
+		roomStatus[roomCounter] = 0;
 		socket.emit('newRoom', roomCounter);
 		roomCounter++;
     });
@@ -37,10 +55,15 @@ io.on('connection', function(socket){
 		sendBufView[1] = msg[2];
 
 		var roomName = msg[0];
+		
 		console.log(sendBufView);
 		console.log('move in room: ' + roomName + ' on position: ' + msg[1] + ' victory: ' + msg[2]);
-
 		socket.broadcast.to(roomName).emit('turn', bufArr);
+		if(msg[2] != 0){
+			roomStatus[roomName] = 2;
+			socket.leave(roomName);
+		}
+		
 	});
 });
 
@@ -57,15 +80,24 @@ io.on('connection', function(socket){
 				socket.emit('err', roomName + ' does not exist');
 				console.log(roomName + " does not exist");
 			}
-			else if(room.length > 1){ 
+			else if(roomStatus[roomName] != 0){ 
 				socket.emit('err', roomName + ' is full');
 				console.log(roomName + " is full");
 			}
 			else{
+				roomStatus[roomName] = 1;
 				console.log('user: ' + player2 + ' connecting to: ' + roomName);
 				socket.join(roomName);
 				socket.broadcast.to(roomName).emit('player1', player2);
 				socket.emit('player2', roomName);
 			}
-    });
+		});
+		
+		io.on('connection', function(socket){
+			socket.on('ackResult', function(msg){
+				console.log('Leaving room ' + msg + '..');
+				socket.leave(msg);
+			});
+	});
+
 });
